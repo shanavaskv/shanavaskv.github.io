c98eff1d2bf3697e55c3577fcea52bea3ef31253
shanavas <shanavaskv@gmail.com>
2016-11-19 20:11:56 -0500

    initial

---
+++ wp-login.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>Shanavas &lsaquo; Log In</title>
+	<link rel='dns-prefetch' href='//s.w.org'/>
+<script type='text/javascript' src='/wp-admin/load-scripts.php?c=0&amp;load%5B%5D=jquery-core,jquery-migrate&amp;ver=4.6.1'></script>
+<script type='text/javascript' src='/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.js?ver=4.3.2'></script>
+<link rel='stylesheet' href='/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.6.1' type='text/css' media='all'/>
+<link rel='stylesheet' id='aqpb-view-css-css' href='/wp-content/plugins/ebor-page-builder-master/assets/css/aqpb-view.css?ver=4.6.1' type='text/css' media='all'/>
+<link rel='stylesheet' id='genericons-css' href='/wp-content/plugins/jetpack/_inc/genericons/genericons/genericons.css?ver=3.1' type='text/css' media='all'/>
+<link rel='stylesheet' id='jetpack-sso-login-css' href='/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.css?ver=4.3.2' type='text/css' media='all'/>
+<meta name='robots' content='noindex,follow'/>
+<link rel="icon" href="http://i0.wp.com/www.shanavasv.com/wp-content/uploads/2016/08/favicon.png?fit=16%2C16" sizes="32x32"/>
+<link rel="icon" href="http://i0.wp.com/www.shanavasv.com/wp-content/uploads/2016/08/favicon.png?fit=16%2C16" sizes="192x192"/>
+<link rel="apple-touch-icon-precomposed" href="http://i0.wp.com/www.shanavasv.com/wp-content/uploads/2016/08/favicon.png?fit=16%2C16"/>
+<meta name="msapplication-TileImage" content="http://i0.wp.com/www.shanavasv.com/wp-content/uploads/2016/08/favicon.png?fit=16%2C16"/>
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us jetpack-sso jetpack-sso-form-display">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Shanavas</a></h1>
+	
+<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email<br/>
+		<input type="text" name="log" id="user_login" class="input" value="" size="20"/></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br/>
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20"/></label>
+	</p>
+			<div id="jetpack-sso-wrap">
+			
+
+			<div id="jetpack-sso-wrap__action">
+				<a rel="nofollow" href="/wp-login.php?action=jetpack-sso" class="jetpack-sso button button-primary"><span><span class="genericon genericon-wordpress"></span> Log in with WordPress.com</span></a>
+									<p>
+						You can now save time spent logging in by connecting your WordPress.com account to Shanavas.					</p>
+							</div>
+
+							<div class="jetpack-sso-or">
+					<span>Or</span>
+				</div>
+
+				<a href="/wp-login.php?jetpack-sso-show-default-form=1" class="jetpack-sso-toggle wpcom">
+					Log in with username and password				</a>
+
+				<a href="/wp-login.php?jetpack-sso-show-default-form=0" class="jetpack-sso-toggle default">
+					Log in with WordPress.com				</a>
+					</div>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"/> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In"/>
+		<input type="hidden" name="redirect_to" value="/wp-admin/"/>
+		<input type="hidden" name="testcookie" value="1"/>
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">function wp_attempt_focus(){setTimeout(function(){try{d=document.getElementById('user_login');d.focus();d.select();}catch(e){}},200);}
+wp_attempt_focus();if(typeof wpOnload=='function')wpOnload();</script>
+
+	<p id="backtoblog"><a href="/">&larr; Back to Shanavas</a></p>
+	
+	</div>
+
+	
+	<link rel='stylesheet' id='jetpack_css-css' href='/wp-content/plugins/jetpack/css/jetpack.css?ver=4.3.2' type='text/css' media='all'/>
+<script type='text/javascript' src='/wp-content/plugins/ebor-page-builder-master/assets/js/aqpb-view.js?ver=4.6.1'></script>
+	<div class="clear"></div>
+	  <div id="bitnami-banner" data-banner-id="6528b">  <style>#bitnami-banner{z-index:100000;height:80px;padding:0px;width:120px;background:transparent;position:fixed;right:0px;bottom:0px;border:0px solid #ededed}#bitnami-banner .bitnami-corner-image-div{position:fixed;right:0px;bottom:0px;border:0px;z-index:100001;height:110px}#bitnami-banner .bitnami-corner-image-div .bitnami-corner-image{position:fixed;right:0px;bottom:0px;border:0px;z-index:100001;height:110px}#bitnami-close-banner-button{height:12px;width:12px;z-index:10000000000;position:fixed;right:5px;bottom:65px;display:none;cursor:pointer}</style>  <img id="bitnami-close-banner-button" src="/bitnami/images/xclose.png.pagespeed.ic.Zei43eohAv.png"/>  <div class="bitnami-corner-image-div">     <a href="/bitnami/index.html" target="_blank">       <img class="bitnami-corner-image" alt="Bitnami" src="/bitnami/images/xcorner-logo.png.pagespeed.ic.6TukXqDtLV.png"/>     </a>  </div>  <script type="text/javascript">//<![CDATA[
+(function(){var banner=document.getElementById("bitnami-banner");var closeButton=document.getElementById("bitnami-close-banner-button");var toggleCloseButton=function toggleCloseButton(open){if(closeButton){if(open&&banner&&banner.style.display!="none"){closeButton.style.display="block";}else{closeButton.style.display="none";}}
+return false;};var setCookie=function setCookie(cname,cvalue,exdays){var d=new Date();d.setTime(d.getTime()+(exdays*24*60*60*1000));var expires="expires="+d.toUTCString();document.cookie=cname+"="+cvalue+"; "+expires;};var closeBanner=function closeBanner(open){if(banner){var suffix=banner.getAttribute('data-banner-id')||'global';banner.style.display="none";setCookie('_bitnami_closed_banner_'+suffix,"1",30);}
+return false;};banner.onmouseover=function(){return toggleCloseButton(1);};banner.onmouseout=function(){return toggleCloseButton(0);};closeButton.onclick=closeBanner;}());
+//]]></script> </div>   </body>
+	</html>
+	
